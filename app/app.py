9973728414b01911979973e23aa7d0d26f2467bb
shpeendz <shpendzuka45@gmail.com>
2026-06-10 11:34:11 +0200

    Post hinzugefügt bei artikel.py

--- app/app.py
+++ app/app.py
@@ -1,35 +1,29 @@
 import os
-from flask import Flask
-
-from routes.bestellungen import bestellungen_bp
-from routes.kunden import kunden_bp
-from routes.lieferungen import lieferungen_bp
-from routes.artikel import artikel_bp
+from flask import Flask, send_from_directory
 from models import db, Lieferant
-
+from routes.artikel import artikel_bp
+from routes.kunden import kunden_bp
+from routes.bestellungen import bestellungen_bp
+from routes.lieferungen import lieferungen_bp
 
 app = Flask(__name__)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_PATH  = os.path.join(BASE_DIR, '..', 'db', 'tante_emma')
+DB_PATH  = os.path.join(BASE_DIR, '..', 'db', 'tante_emma.sqlite')
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_PATH}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'tante-emma-dev'
 
-
 db.init_app(app)
 
 app.register_blueprint(artikel_bp)
-app.register_blueprint(bestellungen_bp)
 app.register_blueprint(kunden_bp)
+app.register_blueprint(bestellungen_bp)
 app.register_blueprint(lieferungen_bp)
-
-
-
 
 @app.route('/')
 def index():
-    return 'Tante Emma läuft!'
+    return send_from_directory('static', 'index.html')
 
 if __name__ == '__main__':
     with app.app_context():
